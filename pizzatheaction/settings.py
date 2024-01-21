9b138f7ccaf724d99ff792fadde98c2c3a0a09e3
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-21 18:55:23 +0000

    Add base css and background image

--- pizzatheaction/settings.py
+++ pizzatheaction/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'pizzatheaction.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -152,6 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
