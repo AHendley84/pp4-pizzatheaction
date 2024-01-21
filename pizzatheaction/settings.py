939f4d8fce3c184891ab0f55848fa71f93ef6fb0
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-21 11:12:50 +0000

    Finish allauth setup

--- pizzatheaction/settings.py
+++ pizzatheaction/settings.py
@@ -88,8 +88,17 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-WSGI_APPLICATION = 'pizzatheaction.wsgi.application'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+WSGI_APPLICATION = 'pizzatheaction.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
