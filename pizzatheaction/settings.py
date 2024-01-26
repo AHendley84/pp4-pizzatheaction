2170df161fe7875289066641aed2b29942ffc834
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-26 06:16:50 +0000

    Amend BASE_DIR path

--- pizzatheaction/settings.py
+++ pizzatheaction/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
-from pathlib import Path
 if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
