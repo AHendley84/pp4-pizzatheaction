53d26a708a55ba1c1c62725949eb459a499cd809
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-26 06:15:06 +0000

    Edit DEBUG to try and understand the error

--- pizzatheaction/settings.py
+++ pizzatheaction/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #'DEVELOPMENT' in os.environ
+#'DEVELOPMENT' in os.environ
+DEBUG = True
 
 if 'DEVELOPMENT' in os.environ:
     ALLOWED_HOSTS = ['127.0.0.1']
