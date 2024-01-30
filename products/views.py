ce01c3a1d7fa3cc9ac5488edf69cac20e4f43768
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-30 19:57:20 +0000

    Add js for filter and back to top button

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Brand, Category
 
 # Create your views here.
@@ -22,11 +24,16 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
 
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-                products = products.order_by(sortkey)
+            products = products.order_by(sortkey)
 
 
         if 'category' in request.GET:
