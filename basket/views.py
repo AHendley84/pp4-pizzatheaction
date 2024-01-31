c1f7c7949495fb7b5b5e36d0a4e228dca1a68f94
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-31 19:22:34 +0000

    Add basket template

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_basket(request):
+    """
+    A view to return the shopping basket and its contents
+    """
+
+    return render(request, 'basket/basket.html')
