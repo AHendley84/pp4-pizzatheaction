ce2a6382bba645ba2f7bc09a87f85b92d3f11800
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-02-06 16:16:12 +0000

    Add checkout success logic and template

--- checkout/models.py
+++ checkout/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('line_item_toal'))['line_item_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('line_item_total'))['line_item_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_charge = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from basket.contexts import basket_contents
 
 import stripe
@@ -13,21 +15,62 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "There are currently no items in your basket")
-        return redirect(reverse('products'))
-    
-    current_basket = basket_contents(request)
-    total = current_basket ['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    order_form = OrderForm()
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'address_line1': request.POST['address_line1'],
+            'address_line2': request.POST['address_line2'],
+            'town_city': request.POST['town_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your basket was not found in our database. "
+                        "Please contact us for assistance.")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+                
+            request.session['save-info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                           Please check the information supplied is correct.')
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There are currently no items in your basket")
+            return redirect(reverse('products'))
+        
+        current_basket = basket_contents(request)
+        total = current_basket ['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key missing. \
@@ -39,6 +82,28 @@ def checkout(request):
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
 
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts.
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully placed! Your order number is: \
+                     {order_number}. \
+                     A confirmation email has been sent to: \
+                     {order.email}.')
+    
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
