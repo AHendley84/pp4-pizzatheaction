5476f97644bd2d72b7ac3630332b704f3b953900
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-07 17:37:21 +0000

    Get 500 error when accessing admin on live site, debug to true to resolve

--- pizzatheaction/settings.py
+++ pizzatheaction/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True #'DEVELOPMENT' in os.environ
 
 if 'DEVELOPMENT' in os.environ:
     ALLOWED_HOSTS = ['8000-ahendley84-pp4pizzathea-0qvu5gaq5i7.ws-eu108.gitpod.io']
