b4359ea646fb097ff96bfa05c4b17c2331b8a421
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-08 15:44:00 +0000

    Add product form to product app

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category, Brand
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_categories = [(c.id, c.get_friendly_name()) for c in categories]
+        brands = Brand.objects.all()
+        friendly_brands = [(b.id, b.get_friendly_name()) for b in brands]
+
+        self.fields['category'].choices = friendly_categories
+        self.fields['brand'].choices = friendly_brands
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-1'
