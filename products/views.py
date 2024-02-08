ba8ccd71ac3f769d72fd087c6a41741827a7848a
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-08 18:35:04 +0000

    Add delete product views, templates and added defensive programming to delete function on products and view_products

--- products/views.py
+++ products/views.py
@@ -118,9 +118,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Product added successfully')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, f'{product.name} added successfully')
+            return redirect(reverse('view_product', args=[product.id]))
         else:
             messages.error(request, 'Product failed submission. Pleas review and ensure it is valid.')
     else:
@@ -158,3 +158,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete a product in the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'{product.name} has been successfully deleted')
+    return redirect(reverse('products'))
