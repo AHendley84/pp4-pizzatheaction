4f894de8f4efb1df7bd6dc632a57854d02e9365e
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-08 12:11:04 +0000

    Add profile form, views and update template

--- checkout/models.py
+++ checkout/models.py
@@ -21,7 +21,7 @@ class Order(models.Model):
     town_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
+    country = CountryField(blank_label='Country', null=False, blank=False)
     order_date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     delivery_charge = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
---
+++ profiles/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+        
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_address_line1': 'Address Line 1',
+            'default_address_line2': 'Address Line 2',
+            'default_town_city': 'Town or City',
+            'default_county': 'County',
+            'default_postcode': 'Postal Code',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'rounded-1 profile-form-input'
+            self.fields[field].label = False
