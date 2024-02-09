e6a9e5521de726de2dc9ebea5d32a07584417737
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-09 13:56:19 +0000

    Add blog views, urls and template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def blog_home(request):
+    """
+    A view to retun the blog page
+    """
+    return render(request, 'blog/blog.html')
