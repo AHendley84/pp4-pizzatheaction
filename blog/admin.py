10f982e4d47da79b52ee61a86d752864a0553448
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-09 14:26:46 +0000

    Add blogpost model and admin to blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'created_by',
+    )
+
+admin.site.register(BlogPost)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=254)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # If the created_by field is not set, and there is a logged-in user,
+        # set the created_by field to the current user.
+        if not self.created_by and hasattr(self, 'request_user'):
+            self.created_by = self.request_user
+        super().save(*args, **kwargs)
