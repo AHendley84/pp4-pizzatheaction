135060cb0d2eda4d05b2a3f4f20a3d1973a81442
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-09 11:11:05 +0000

    Add description to category model in products app

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=100)
     friendly_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
