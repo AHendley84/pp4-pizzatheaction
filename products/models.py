303b039d12f033f0bdc2dcac0f9054d5b6335ad8
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-13 19:06:52 +0000

    Amend product models to include new fields for dimensions, fuel and weight

--- products/models.py
+++ products/models.py
@@ -37,6 +37,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=254, null=True, blank=True)
+    fuel = models.CharField(max_length=20, null=True, blank=True)
+    weight = models.CharField(max_length=40, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
