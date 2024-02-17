baad10b2e53f5b21dad3830629a56c039b52c647
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-17 14:10:11 +0000

    Refacrtor: Adjust line 101 on checkout models.py preventing heroku building the app

--- checkout/models.py
+++ checkout/models.py
@@ -98,8 +98,9 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(Sum(
             'line_item_total'))['line_item_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            (self.delivery_charge=self.order_total *
-                settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            self.delivery_charge = (
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+                )
         else:
             self.delivery_charge = 0
         self.grand_total = self.order_total + self.delivery_charge
