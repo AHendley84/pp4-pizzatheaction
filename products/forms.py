0cca1bea46a2e4a4dd66ce431ad6e0501b4ecc79
Andrew Hendley <andrewhendley84@gmail.com>
2024-02-21 19:00:25 +0000

    Debug: Identified error with custom clearable file input. Issue resolved

--- products/forms.py
+++ products/forms.py
@@ -26,4 +26,4 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_categories
         self.fields['brand'].choices = friendly_brands
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'rounded-1'
+            field.widget.attrs['class'] = 'rounded-1 new-image'
